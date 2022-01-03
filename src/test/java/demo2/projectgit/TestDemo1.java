7c6ee346ea147c1a48d622e474ae9b3f33deeb59
shilpa_bhadra <shilpa.bhadran@gmail.com>
2022-01-03 16:56:21 +0530

    all files added

---
+++ src/test/java/demo2/projectgit/TestDemo1.java
@@ -0,0 +1,12 @@
+package demo2.projectgit;
+
+import org.testng.annotations.Test;
+
+public class TestDemo1 {
+
+@Test	
+public void demoOne() {
+	System.out.println("this is testDemo1");
+
+}
+}
---
+++ src/test/java/demo2/projectgit/TestDemo2.java
@@ -0,0 +1,17 @@
+package demo2.projectgit;
+
+import org.testng.annotations.Test;
+
+public class TestDemo2 {
+
+	@Test
+	public void demoTwo() {
+		System.out.println("this is testDemo2");
+
+	}	
+	
+	
+	
+	
+	
+}
